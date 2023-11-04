9d4df95394fca229a93023408204f2e25333e4c3
Sebastian Pitur <seba_pitur@yahoo.com>
2023-11-04 19:51:23 +0200

    remove application-context.xml and add annotations

--- src/main/java/BankApplication.java
+++ src/main/java/BankApplication.java
@@ -9,8 +9,12 @@ import com.luxoft.bankapp.service.Banking;
 import com.luxoft.bankapp.service.BankingImpl;
 import com.luxoft.bankapp.model.Client.Gender;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.luxoft.bankapp")
 public class BankApplication {
 
     private static final String[] CLIENT_NAMES =
@@ -18,7 +22,7 @@ public class BankApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BankApplication.class);
         Banking banking = initialize(context);
 
         workWithExistingClients(banking);
